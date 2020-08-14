29d4029417ec826b40b3a0280db65f8d8e1c923e
zickalate <zickalate.jk@gmail.com>
2020-08-14 15:11:36 +0700

    Create Exercise5_1_Phatcha_K.py

---
+++ assignments/Exercise5_1_Phatcha_K.py
@@ -0,0 +1,7 @@
+number1 = float(input("First Number : "))
+number2 = float(input("Second Number : "))
+
+print(number1, "+", number2, "=", number1+number2)
+print(number1, "-", number2, "=", number1-number2)
+print(number1, "x", number2, "=", number1*number2)
+print(number1, "/", number2, "=", number1/number2)
