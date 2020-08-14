bbfa897e79932a360eea467fec9e9752242c3a8f
zickalate <zickalate.jk@gmail.com>
2020-08-14 13:36:02 +0700

    Create Exercise4_Phatcha_K.py

---
+++ assignments/Exercise4_Phatcha_K.py
@@ -0,0 +1,11 @@
+EngScore = 95
+BusinessScore = 90
+ComsystemScore = 92
+ProgrammingScore = 96
+
+print("--- Your Score ---")
+print("Foundation English :", EngScore)
+print("General Business :", BusinessScore)
+print("Introduction to Computer Systems :", ComsystemScore)
+print("Computer Programming :", ProgrammingScore)
+
