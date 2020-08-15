c6d24e5a79a08ea43e0200b5c5d97e0392698071
zickalate <zickalate.jk@gmail.com>
2020-08-15 23:28:44 +0700

    Create Lecture73_Phatcha_K.py

---
+++ assignments/Lecture73_Phatcha_K.py
@@ -0,0 +1,17 @@
+systemMenu = {"ไก่ทอด": 35, "เป็ดทอด": 45, "ปลาทอด": 55, "ผักทอด": 20}
+menuList = []
+def showBill():
+    totalPrice = 0
+    print("---- My Food----")
+    for number in range(len(menuList)):
+        print(menuList[number][0], menuList[number][1])
+        totalPrice += int(menuList[number][1])
+    print("Total : ", totalPrice)
+while True:
+    menuName = input("Plese Enter Menu : ")
+    if(menuName.lower() == "exit"):
+        break
+    else:
+        menuList.append([menuName, systemMenu[menuName]])
+
+showBill()
