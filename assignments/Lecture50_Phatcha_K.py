8483a2e4d6d6884899c017f67e18e72901a2a439
zickalate <zickalate.jk@gmail.com>
2020-08-15 15:53:28 +0700

    Create Lecture50_Phatcha_K.py

---
+++ assignments/Lecture50_Phatcha_K.py
@@ -0,0 +1,13 @@
+def plusNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+
+plusNumber(20,10)
+minusNumber(20,10)
+multiplyNumber(20,10)
+divideNumber(20,10)
